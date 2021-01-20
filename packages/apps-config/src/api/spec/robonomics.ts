6f3587fd1bc9cecafeea668470a6fa29e5939f9f
Alexander Krupenkin <mail@akru.me>
2021-01-20 10:25:08 +0300

    Drop unused types from robonomics spec

--- packages/apps-config/src/api/spec/robonomics.ts
+++ packages/apps-config/src/api/spec/robonomics.ts
@@ -12,34 +12,4 @@ export default {
   EconomicalParam: '{}',
   ProofParam: 'MultiSignature',
   LiabilityIndex: 'u64',
-  PersistedValidationData: {
-    parent_head: 'Vec<u8>',
-    block_number: 'u32',
-    hrmp_mqc_heads: 'Vec<(u32, Hash)>',
-    dmq_mqc_head: 'Hash',
-    max_pov_size: 'u32'
-  },
-  TransientValidationData: {
-    max_code_size: 'u32',
-    max_head_data_size: 'u32',
-    balance: 'Balance',
-    code_upgrade_allowed: 'Option<u32>',
-    dmq_length: 'u32'
-  },
-  ValidationData: {
-    persistent: 'PersistedValidationData',
-    transient: 'TransientValidationData'
-  },
-  InboundHrmpMessage: {
-    send_at: 'u32',
-    data: 'Vec<u8>'
-  },
-  InboundDownwardMessage: {
-    send_at: 'u32',
-    msg: 'Vec<u8>'
-  },
-  MessageIngestionType: {
-    downward_messages: 'Vec<InboundDownwardMessage>',
-    horizontal_messages: 'Vec<InboundHrmpMessage>'
-  }
 };
